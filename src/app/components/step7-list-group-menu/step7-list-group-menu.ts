7981da64fea31fb39634452ab19603b55734f902
Konstantina Melikoki <konstantina.melik@gmail.com>
2026-04-28 21:02:46 +0300

    feat: add datatable

--- src/app/components/step7-list-group-menu/step7-list-group-menu.ts
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -13,5 +13,6 @@ export class Step7ListGroupMenu {
     { text: "Step3 Component Input", appLink:'component-input-example'},
     { text: "Step4 @for Directive", appLink:'for-directive-example'},
     { text: "Step5 Event Bind", appLink:'event-bind-example'},
+    { text: "Step8 Simple Datatable Example", appLink: 'simple-datatable-example'}
   ]
 }
--- src/app/shared/interfaces/person.ts
+++ src/app/shared/interfaces/person.ts
@@ -3,3 +3,36 @@ export interface IPerson {
     lastname: string;
     email: string;
 }
+
+export const ManyPersons: IPerson[] = [
+    {"firstname":"Karina","lastname":"Firidolfi","email":"kfiridolfi0@bizjournals.com"},
+    {"firstname":"Reinaldos","lastname":"Marjot","email":"rmarjot1@mediafire.com"},
+    {"firstname":"Vince","lastname":"Stroyan","email":"vstroyan2@nationalgeographic.com"},
+    {"firstname":"Kiley","lastname":"Osant","email":"kosant3@cpanel.net"},
+    {"firstname":"Damien","lastname":"Sant","email":"dsant4@google.com"},
+    {"firstname":"Deina","lastname":"Fairn","email":"dfairn5@wired.com"},
+    {"firstname":"Luigi","lastname":"Sadler","email":"lsadler6@washingtonpost.com"},
+    {"firstname":"Ardella","lastname":"Petroff","email":"apetroff7@bloglines.com"},
+    {"firstname":"Matteo","lastname":"Toler","email":"mtoler8@qq.com"},
+    {"firstname":"Kristos","lastname":"Tubridy","email":"ktubridy9@rakuten.co.jp"},
+    {"firstname":"Gwyn","lastname":"Ambrosch","email":"gambroscha@indiatimes.com"},
+    {"firstname":"Don","lastname":"Winsley","email":"dwinsleyb@usnews.com"},
+    {"firstname":"Rayna","lastname":"Hatzar","email":"rhatzarc@dailymail.co.uk"},
+    {"firstname":"Rosabel","lastname":"Gadault","email":"rgadaultd@apple.com"},
+    {"firstname":"Ermin","lastname":"Ferraresi","email":"eferraresie@sciencedirect.com"},
+    {"firstname":"Wendel","lastname":"Riddle","email":"wriddlef@businessweek.com"},
+    {"firstname":"Rasla","lastname":"Pyett","email":"rpyettg@angelfire.com"},
+    {"firstname":"Aldwin","lastname":"Tommei","email":"atommeih@facebook.com"},
+    {"firstname":"Arney","lastname":"Stoacley","email":"astoacleyi@cdc.gov"},
+    {"firstname":"Holden","lastname":"Elgy","email":"helgyj@yahoo.co.jp"},
+    {"firstname":"Shepard","lastname":"Jales","email":"sjalesk@rakuten.co.jp"},
+    {"firstname":"Christopher","lastname":"Monelle","email":"cmonellel@washington.edu"},
+    {"firstname":"Jacenta","lastname":"Scyone","email":"jscyonem@amazon.de"},
+    {"firstname":"Layne","lastname":"Ivakin","email":"livakinn@state.gov"},
+    {"firstname":"Marcellus","lastname":"McDugal","email":"mmcdugalo@slate.com"},
+    {"firstname":"Raleigh","lastname":"Selwood","email":"rselwoodp@scientificamerican.com"},
+    {"firstname":"Adelaida","lastname":"Scargill","email":"ascargillq@google.fr"},
+    {"firstname":"George","lastname":"Klimowicz","email":"gklimowiczr@gmpg.org"},
+    {"firstname":"Charlotte","lastname":"Hallwell","email":"challwells@bravesites.com"},
+    {"firstname":"Deane","lastname":"Cliffe","email":"dcliffet@shinystat.com"}
+  ]
